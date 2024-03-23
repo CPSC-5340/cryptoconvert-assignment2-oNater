329ef41f2dbe75b97c5f8ef5324d20555813cdf0
oNater <127701018+oNater@users.noreply.github.com>
2024-03-22 23:08:46 -0700

    Update CryptoExchangeView.swift

--- Assignment2/CryptoExchangeView.swift
+++ Assignment2/CryptoExchangeView.swift
@@ -1,4 +1,3 @@
-//
 //  CryptoExchangeView.swift
 //  Assignment2
 //
@@ -8,10 +7,26 @@
 import SwiftUI
 
 struct CryptoExchangeView: View {
+    @StateObject private var viewModel = CryptoExchangeViewModel()
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("Enter number of Bitcoins", text: $viewModel.bitcoinAmount)
+                .padding()
+                .keyboardType(.decimalPad)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+
+            List(viewModel.convertedValues, id: \.0) { currency, value in
+                Text("BTC to \(currency): \(String(format: "%.2f", value))")
+            }
+
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Crypto Exchange")
     }
 }
+
 
 #Preview {
     CryptoExchangeView()
