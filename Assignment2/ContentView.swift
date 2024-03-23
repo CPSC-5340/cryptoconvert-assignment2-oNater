53ff9bc8e00c62926a99343ce14dd22bdc2ac9a0
oNater <127701018+oNater@users.noreply.github.com>
2024-03-22 23:06:03 -0700

    Update ContentView.swift

--- Assignment2/ContentView.swift
+++ Assignment2/ContentView.swift
@@ -7,20 +7,46 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
         }
-        .padding()
     }
 }
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 20) {
+                Text("Converter App")
+                    .font(.largeTitle)
+                    .padding()
+                
+                NavigationLink(destination: CurrencyExchangeView()) {
+                    Text("World Currency Exchange")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                }
+                
+                NavigationLink(destination: CryptoExchangeView()) {
+                    Text("Crypto Exchange")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.green)
+                        .cornerRadius(8)
+                }
+            }
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
