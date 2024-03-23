a8479e67b51c2f6cefc720cc53df2fba2a20279f
oNater <127701018+oNater@users.noreply.github.com>
2024-03-22 23:08:15 -0700

    Update CryptoExchangeViewModel.swift

--- Assignment2/CryptoExchangeViewModel.swift
+++ Assignment2/CryptoExchangeViewModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+class CryptoExchangeViewModel: ObservableObject {
+    @Published var bitcoinAmount: String = ""
+    
+    let conversionRates = [
+        "USD": 42000.00, 
+        "ETH": 14.0,
+        "Tether": 42000,
+        "BNB": 120.0,
+        "USD Coin": 42000,
+        "XRP": 39200.0
+    ]
+    
+    var convertedValues: [(String, Double)] {
+        guard let bitcoin = Double(bitcoinAmount) else { return [] }
+        return conversionRates.map { ($0.key, $0.value * bitcoin) }
+    }
+}
